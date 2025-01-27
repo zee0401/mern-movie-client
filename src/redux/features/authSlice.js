8d86d0540ac4ff4a2d9a10c1f37aa533b521145c
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-27 15:14:20 +0530

    verify auth on first render

--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -1,23 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
 
 const authSlice = createSlice({
   name: "auth",
   initialState: {
     adminDetails: null,
-    isAuthenticated: false,
+    isAuthenticated: !!Cookies.get("jwt"),
   },
   reducers: {
     login: (state, action) => {
       state.adminDetails = action.payload.adminDetails;
       state.isAuthenticated = true;
+      Cookies.set("jwt", action.payload.token);
     },
     logout: (state) => {
       state.adminDetails = null;
       state.isAuthenticated = false;
+      Cookies.remove("jwt");
+    },
+    setAuthenticated: (state, action) => {
+      state.isAuthenticated = action.payload;
     },
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout, setAuthenticated } = authSlice.actions;
 
 export default authSlice.reducer;
