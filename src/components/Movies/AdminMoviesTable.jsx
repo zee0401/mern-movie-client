4c44cfb7e891153f7836bd609b0e2409c0b6b70a
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-29 18:08:21 +0530

    admin all movies page

--- src/components/Movies/AdminMoviesTable.jsx
+++ src/components/Movies/AdminMoviesTable.jsx
@@ -1,24 +1,9 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Select,
-  MenuItem,
-  FormControl,
-  InputLabel,
-} from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { getAllMovies } from "../../api/moviesApi";
-import BorderColorRoundedIcon from "@mui/icons-material/BorderColorRounded";
-import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
-import { useState } from "react";
+
+import MoviesTableContainer from "./MoviesTableContainer";
 
 const MovieTable = () => {
-  const [sortBy, setSortBy] = useState("rating");
   const {
     data: movies = [],
     isLoading,
@@ -27,12 +12,6 @@ const MovieTable = () => {
     queryKey: ["movies"],
     queryFn: getAllMovies,
   });
-
-  console.log(movies);
-
-  const handleSortChange = (event) => {
-    setSortBy(event.target.value);
-  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -44,56 +23,7 @@ const MovieTable = () => {
 
   return (
     <div>
-      <FormControl fullWidth style={{ marginBottom: "20px" }}>
-        <InputLabel id="sort-by-label">Sort By</InputLabel>
-        <Select
-          labelId="sort-by-label"
-          id="sort-by-select"
-          value={sortBy}
-          label="Sort By"
-          onChange={handleSortChange}
-        >
-          <MenuItem value="rating">Rating</MenuItem>
-          <MenuItem value="releaseDate">Release Date</MenuItem>
-          <MenuItem value="title">Title</MenuItem>
-        </Select>
-      </FormControl>
-
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Image</TableCell>
-              <TableCell>Title</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell>Rating</TableCell>
-              <TableCell>Release Date</TableCell>
-              <TableCell>Update</TableCell>
-              <TableCell>Delete</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {movies.map((movie) => (
-              <TableRow key={movie._id || movie.id}>
-                {" "}
-                <TableCell>
-                  <img src={movie.image} alt={movie.name} width="60" />
-                </TableCell>
-                <TableCell>{movie.name}</TableCell>
-                <TableCell>{movie.description}</TableCell>
-                <TableCell>{movie.rating}</TableCell>{" "}
-                <TableCell>{movie.releaseDate}</TableCell>
-                <TableCell>
-                  <BorderColorRoundedIcon color="edit" />
-                </TableCell>
-                <TableCell>
-                  <DeleteForeverRoundedIcon color="delete" />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <MoviesTableContainer movies={movies} />
     </div>
   );
 };
