1c546db61410e930da441dc681dea91c722240fc
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-04 01:55:07 +0530

    get all movies at home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import { Box, Stack } from "@mui/material";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getAllMovies } from "../api/moviesApi";
+import { Stack } from "@mui/material";
 import Movies from "../components/Movies/Movies";
 import SearchBox from "../components/Search/SearchBox";
 import SortByDropdown from "../components/sort/SortBy";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const searchClick = () => {
+    navigate(`/search-movies?search=${searchTerm}`);
+  };
+
+  const {
+    data: movies = [],
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["movies"],
+    queryFn: getAllMovies,
+  });
+
   return (
     <div>
       <Stack
@@ -13,11 +33,21 @@ const Home = () => {
         width="100%"
         sx={{ marginBottom: "1rem" }}
       >
-        <SearchBox style={{ width: "100%" }} />
-
+        <SearchBox
+          style={{ width: "100%" }}
+          searchTerm={searchTerm}
+          onSearchChange={setSearchTerm}
+          onSearchClick={searchClick}
+        />
         <SortByDropdown style={{ width: "100%" }} />
       </Stack>
-      <Movies />
+
+      <Movies
+        movies={movies}
+        isLoading={isLoading}
+        error={error}
+        searchTerm={searchTerm}
+      />
     </div>
   );
 };
