a671442914aa95a7d2dc7950223ee6279ae5489b
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-04 17:46:28 +0530

    added browse movie page

--- src/pages/BrowseMovies.jsx
+++ src/pages/BrowseMovies.jsx
@@ -1,41 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { useSearchParams } from "react-router";
+import { useSearchParams } from "react-router-dom";
 import { searchMovies } from "../api/moviesApi";
 import Movies from "../components/Movies/Movies";
 import { Stack } from "@mui/material";
 import SearchBox from "../components/Search/SearchBox";
 import SortByDropdown from "../components/sort/SortBy";
 
-const Search = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+const BrowseMovies = () => {
+  const [params] = useSearchParams();
 
-  const params = searchParams.get("search");
-  const sortParams = searchParams.get("sortBy");
-
-  const [searchTerm, setSearchTerm] = useState(params);
-  const [sortBy, setSortBy] = useState("rating");
-
-  const onSearchClick = () => {
-    setSearchParams({ search: searchTerm, sortBy: sortBy });
-  };
+  const searchParams = params.get("search") || "";
+  const sortParams = params.get("sortBy") || "rating";
 
   const {
     data: movies = [],
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["movies", params, sortBy],
-    queryFn: () => searchMovies(params, sortParams),
+    queryKey: ["movies", searchParams, sortParams],
+    queryFn: () => searchMovies(searchParams, sortParams),
   });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
@@ -46,26 +33,16 @@ const Search = () => {
         width="100%"
         sx={{ marginBottom: "1rem" }}
       >
-        <SearchBox
-          style={{ width: "100%" }}
-          searchTerm={searchTerm}
-          onSearchChange={setSearchTerm}
-          onSearchClick={onSearchClick}
-        />
-        <SortByDropdown
-          style={{ width: "100%" }}
-          sortBy={sortParams}
-          setSortBy={setSortBy}
-          searchTerm={searchTerm}
-        />
+        <SearchBox />
+        <SortByDropdown />
       </Stack>
       {movies.length === 0 ? (
         <div>No movies found</div>
       ) : (
-        <Movies movies={movies} isLoading={isLoading} error={error} />
+        <Movies movies={movies} />
       )}
     </div>
   );
 };
 
-export default Search;
+export default BrowseMovies;
