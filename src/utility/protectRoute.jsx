c01a126c0b0d6e6370c83abd16038a5dcf2726d5
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-05 19:19:50 +0530

    protected route

--- src/utility/protectRoute.jsx
+++ src/utility/protectRoute.jsx
@@ -1,11 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = () => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const isChecking = useSelector((state) => state.auth.isChecking);
+
+  if (isChecking) {
+    return <div>Loading...</div>;
+  }
 
   if (!isAuthenticated) {
-    return <Navigate to="/" replace={true} />;
+    return <Navigate to="/" replace />;
   }
 
   return <Outlet />;
